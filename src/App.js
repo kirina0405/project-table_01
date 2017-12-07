5e842af4d1da3af2bb9f6759a5007d4f8df1aca9
Iryna Pikalova <kirina0405@gmail.com>
2017-12-07 19:08:02 +0200

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
-// import FieldForText from './Components/FieldForText';
-// import Table from './Components/Table';
 
 export default class App extends Component {
     constructor(props) {
@@ -21,10 +18,10 @@ export default class App extends Component {
     
     handleSubmit(event) {
         const loadText = this.state.value;
-        // loadText = 'First name, Second name, Phone, E-mail, Web; John, Forest, +380979804567, JF@gmail.com, www.JohnForest.com; Iohn, MarkUp, +380975554567, IM@gmail.com, www.IohnMarkUp.com';
-        const arr = loadText.split(';');
+        // loadText = 'First name, Second name, Phone, E-mail, Web John, Forest, +380979804567, JF@gmail.com, www.JohnForest.com Iohn, MarkUp, +380975554567, IM@gmail.com, www.IohnMarkUp.com';
+        const arr = loadText.split('\n');
         const arrThead = arr.splice(0, 1);
-        const theadRow = arrThead.join('\n').split(',')
+        const theadRow = arrThead.join('\n').split(',');
         this.setState({
             headTable: theadRow,
             bodyTable: arr
@@ -32,53 +29,43 @@ export default class App extends Component {
     }  
     render() {
         return (
-        <section>
-            <header>
-            <h2 className='fieldForText__title'>Load your text</h2>
-            <div className='fieldForText__field'>
-                <textarea 
-                rows = '5'
-                cols = '45'
-                type = 'text'
-                placeholder = 'Text...'
-                value = {this.state.value}
-                onChange = {this.handleChange}>
-                </textarea>
-            </div>
-            <button className='fieldForText__button' onClick = {this.handleSubmit}>
-            Get your table </button>
-            </header>
-            <main>
-                <table>
-                    <thead>
-                    <tr>
-                        {
-                            this.state.headTable ? this.state.headTable.map(item => <th key={item}>{item}</th>) : null
-                        }
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.bodyTable
-                        ? this.state.bodyTable.map(row => {
-                            return (<tr key={row.split(',')}>{
-                                row.split(',').map(cell=>{
-                                    return <td key={cell}>{cell}</td>
-                                })
-                            }</tr>
-                            )
-                        })
-                        : null
-                    }
-                </tbody>  
-                </table> 
-            </main>
-        </section>
+            <section>
+                <header>
+                    <p className='fieldForText_title'>Load your text</p>
+                    <div className='triangle'></div>
+                    <div className='fieldForText__field'>
+                        <textarea 
+                        rows = '10'
+                        cols = '65'
+                        type = 'text'
+                        placeholder = 'Text...'
+                        value = {this.state.value}
+                        onChange = {this.handleChange}>
+                        </textarea>
+                    </div>
+                    <button className='fieldForText__button' onClick = {this.handleSubmit}>
+                    Get your table </button>
+                    <div className='triangle_1'></div>
+                </header>
+                <main>
+                    <div className='table'>
+                        <table>
+                            <thead>
+                                <tr>
+                                    {this.state.headTable ? this.state.headTable.map(item => 
+                                    <th className='thead' key={item}>{item}</th>) : null}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    {this.state.bodyTable ? this.state.bodyTable.map(row => {
+                                        return (<tr key={row.split(',')}> {
+                                                row.split(',').map(cell => {
+                                                    return <td key={cell}>{cell}</td>})}</tr>)}) : null}
+                            </tbody>  
+                        </table>            
+                    </div>
+                </main>
+            </section>
         )
     }
-
 }
-
-
-
-
